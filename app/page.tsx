ca48d1091550f9382a2696c35c9643c9e3652721
draihan <draihan@student.ubc.ca>
2023-11-18 14:25:01 -0800

    Structure Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,119 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  MotionValue,
+  motion,
+  useMotionTemplate,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+import "@/styles/globals.css";
+import { useRef } from "react";
 
 export default function Home() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end end"],
+  });
+  const scrollYProgressSpring = useSpring(scrollYProgress, {
+    stiffness: 300,
+    damping: 40,
+  }) as MotionValue<number>;
+  const scale = useTransform(scrollYProgressSpring, [0, 1], [1, 12]);
+  const imageX = useTransform(scrollYProgressSpring, [0, 1], [50, 0]);
+  const imageXCalc = useMotionTemplate`max(0px, calc(${imageX}% + calc(${imageX}vw - 300px)))`;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <div ref={ref} className="relative z-10 h-[200vh] overflow-clip">
+        <motion.div
+          style={{ scale }}
+          className="hero-background sticky left-0 top-0 grid h-screen origin-[50%_70%] gap-2 p-6 pt-12 [grid-template-rows:4fr_1fr] 
+          md:origin-[90%_40%] md:pt-20"
+        >
+          <div className="window-mask flex flex-col rounded-3xl bg-slate-50 p-12 md:flex-row">
+            <div className="flex h-full flex-col">
+              {/* Title */}
+              <div className="md:mb-auto mb-1 ">
+                <p
+                  className="text-2xl max-w-[12ch] font-md leading-loose  font-sans font-semibold py-2
+                  bg-gradient-to-r from-cyan-800 to-orange-950 inline-block text-transparent bg-clip-text
+                  md:text-4xl 
+                  xl:text-6xl
+                  "
+                >
+                  Dany Raihan
+                </p>
+                <p className="text-lg text-slate-500 font-md ml-1 border-t-[2px] border-slate-200">
+                  2nd Year Major in Computer Science at UBC
+                </p>
+              </div>
+
+              {/* Desc */}
+              <div className="text-md space-y-1 pl-1 ">
+                <p className="border-b-[1px] border-slate-200 mb-1">
+                  Full-Stack Developer
+                </p>
+
+                <a
+                  href="mailto:dannyraihan123@gmail.com"
+                  className="bg-gradient-to-r from-sky-900 via-red-900 to-green-900 text-transparent bg-clip-text"
+                >
+                  dannyraihan123@gmail.com
+                </a>
+                <br />
+                <a
+                  href="https://www.linkedin.com/in/dany-raihan/"
+                  target="_blank"
+                  className="bg-gradient-to-r from-cyan-900 to-blue-900 text-transparent bg-clip-text"
+                >
+                  linkedin/dany-raihan
+                </a>
+                <br />
+                <a
+                  href="tel:+12369958234"
+                  className="bg-gradient-to-r from-cyan-500 to-blue-700 text-transparent bg-clip-text"
+                >
+                  (236) 995 - 8234
+                </a>
+                <br />
+              </div>
+            </div>
+
+            {/* Scroll hole */}
+            <div
+              className="mx-auto -mb-7 mt-4 box-content h-[90%] w-[100%] min-w-[150px] 
+              md:my-auto md:-mr-1 md:ml-auto md:w-[300px] md:min-w-[300px]"
             />
-          </a>
-        </div>
+          </div>
+        </motion.div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Stuff inside hole */}
+      <div
+        className="mt-[-200vh] h-[200vh] overflow-clip  
+        bg-gradient-to-t from-sky-400 to-blue-500"
+      >
+        <motion.div
+          style={{ x: imageXCalc }}
+          className="sticky top-1/2 mx-auto aspect-video w-[1600px] max-w-[90%] rounded-[60px] bg-white shadow-2xl 
+          justify-center items-center flex
+          md:top-1/4"
+        >
+          <div className="w-[90%] h-[90%] flex  justify-center">
+            <div className="px-10 h-fit pb-4 border-b-[1px] border-slate-400">
+              <p className="text-4xl ">Projects</p>
+            </div>
+          </div>
+        </motion.div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Rest  */}
+      <div className="space-y-[80px] bg-gradient-to-b from-sky-400 to-blue-500 md:text-[300px]">
+        <p>such a great guy</p>
       </div>
     </main>
-  )
+  );
 }
