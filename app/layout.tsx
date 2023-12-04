59dc66f6d843181a4d2220703fb3fe0338abd101
draihan <draihan@student.ubc.ca>
2023-12-03 23:30:42 -0800

    optimize font weight import

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "@/styles/globals.css";
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["200", "300", "400", "600", "700"],
 });
 
 export const metadata: Metadata = {
